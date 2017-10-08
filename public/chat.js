f84773a9f8e3d5de95d84bab78c998f8a44e59de
fodnb <Daniel.Mcghee6813@gmail.com>
2017-10-08 16:31:11 -0400

    save session user and updates to chat js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 $(document).ready(()=>{
-
+var handle;
 
 const socket = io.connect("https://boiling-earth-58619.herokuapp.com" || "http://localhost:3500");
 
@@ -12,13 +12,36 @@ $("#message").on('keypress', ()=>{
 
 
 $("#button").on('click', ()=>{
+	handle = $("#handle").val();
+	$("#form").empty();
+	var chatSpace = $("<input>");
+	chatSpace.attr("id", "message");
+	chatSpace.attr("message", "message");
+	chatSpace.attr("message", "message");
+	$("#form").append(chatSpace);
+	var submitMessage = $("<button>");
+	submitMessage.attr('id',  "submitMessage");
+	submitMessage.attr('value', "submit");
+	submitMessage.html('Submit');
+	$("#form").append(submitMessage);
 
-	var handle = $("#handle").val();
+
+	// var handle = $("#handle").val();
+		console.log(handle);
+
+	});
+
+
+$("#form").on('click', '#submitMessage', ()=>{
+	if( $("#message").val() != ""){
 	var message = $("#message").val();
 	socket.emit('chat', {handle, message});
-	$("#handle").val("");	
+	// $("#handle").val("");	
 	$("#message").val("");
-	});
+	console.log(handle);
+	console.log("hi");
+}
+});
 
 socket.on('chat', (data)=>{
 	var newChat = $("<p>");
