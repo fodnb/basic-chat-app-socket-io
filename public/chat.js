8306d9cbcf1563e94738f026ebd74a52073b6c3e
fodnb <Daniel.Mcghee6813@gmail.com>
2017-10-08 18:00:04 -0400

    added comments to chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,48 +1,45 @@
 $(document).ready(()=>{
-var handle;
 
+// set variable for the user during the session	
+var handle;
+// connects to the socket either by url or localhost
 const socket = io.connect("https://boiling-earth-58619.herokuapp.com" || "http://localhost:3500");
 
-$("#message").on('keypress', ()=>{
-	const handle = $("#handle").val();
-	socket.emit('typing', {handle});
 
+// when you submit the button for user name it saves your handle and creates an input for messages and a button to submit your message.
+$("#button").on('click', ()=>{
+	if($("#handle").val() != '' ){
+		handle = $("#handle").val();
+		$("#form").empty();
+		var chatSpace = $("<input>");
+		chatSpace.attr("id", "message");
+		chatSpace.attr("message", "message");
+		chatSpace.attr("message", "message");
+		$("#form").append(chatSpace);
+		var submitMessage = $("<button>");
+		submitMessage.attr('id',  "submitMessage");
+		submitMessage.attr('value', "submit");
+		submitMessage.html('Submit');
+		$("#form").append(submitMessage);
+	}
 });
 
 
+//here we send users in chat the info that someone is typing.  by emitting this to the server wre can handle the information and broadcast to everyone else
+$("#form").on('keypress', $("#message"), ()=>{
+	socket.emit('typing', {handle});
+});	
 
-$("#button").on('click', ()=>{
-	handle = $("#handle").val();
-	$("#form").empty();
-	var chatSpace = $("<input>");
-	chatSpace.attr("id", "message");
-	chatSpace.attr("message", "message");
-	chatSpace.attr("message", "message");
-	$("#form").append(chatSpace);
-	var submitMessage = $("<button>");
-	submitMessage.attr('id',  "submitMessage");
-	submitMessage.attr('value', "submit");
-	submitMessage.html('Submit');
-	$("#form").append(submitMessage);
-
-
-	// var handle = $("#handle").val();
-		console.log(handle);
-
-	});
-
-
+// on this click we submit the message back to the server and then send to all socket connections
 $("#form").on('click', '#submitMessage', ()=>{
 	if( $("#message").val() != ""){
 	var message = $("#message").val();
 	socket.emit('chat', {handle, message});
-	// $("#handle").val("");	
 	$("#message").val("");
-	console.log(handle);
-	console.log("hi");
 }
 });
 
+// when we receive the 'chat' message we add the message to the chat div when we receive the message we delete the message that someone is typing
 socket.on('chat', (data)=>{
 	var newChat = $("<p>");
 	newChat.html(`${data.handle}: ${data.message}`);
@@ -51,6 +48,8 @@ socket.on('chat', (data)=>{
 
 });
 
+
+// when we receive notification that 'typing' then we add a comment to the screen that a specific user is typing.
 socket.on('typing', (data)=>{
 	$("#typing").html(`${data.handle} is typing`);
 });
